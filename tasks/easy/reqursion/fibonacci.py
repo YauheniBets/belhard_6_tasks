25780a97a10137628ab199d2449ab4227d425491
Evgeniy Bets <n3shta2007@yandex.ru>
2021-09-28 13:56:43 +0300

    решено

--- tasks/easy/reqursion/fibonacci.py
+++ tasks/easy/reqursion/fibonacci.py
@@ -1,3 +1,11 @@
 """
 Написать рекурсивную функцию fibonacci, которая будет возвращать n-ый элемент
 """
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+for i in range(50):
+    print(fibonacci(i))
