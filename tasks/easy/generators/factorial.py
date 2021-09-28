4bfcfb57c321d1566919e8aa1832b0e8f34d7d7e
Evgeniy Bets <n3shta2007@yandex.ru>
2021-09-28 12:08:13 +0300

    решено

--- tasks/easy/generators/factorial.py
+++ tasks/easy/generators/factorial.py
@@ -10,3 +10,12 @@ next(factorial_gen) -> 2
 next(factorial_gen) -> 6
 next(factorial_gen) -> 24
 """
+
+def factorial(n: int) -> int:
+    count = 1
+    for i in range(1, n + 1):
+        count *= i
+        yield count
+
+for i in factorial(6):
+    print(i)
