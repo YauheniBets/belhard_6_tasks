2c3059700406338b3d342d6b761a31488a1c523e
Evgeniy Bets <n3shta2007@yandex.ru>
2021-09-28 12:24:32 +0300

    решено

--- tasks/easy/generators/triangular_numbers.py
+++ tasks/easy/generators/triangular_numbers.py
@@ -17,3 +17,12 @@ next(tn_gen) -> 10
 next(tn_gen) -> 15
 next(tn_gen) -> 21
 """
+def triangular_numbers(end_number):
+
+    tn = int(1 / (2 * end_number * (end_number + 1)))
+    for i in range(1, end_number):
+        yield tn
+        tn += i
+
+for i in triangular_numbers(10):
+    print(i)
