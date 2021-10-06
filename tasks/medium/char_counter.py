5f34db5651f3f33ffdb2cf46eb9e0363f63d71e0
Evgeniy Bets <n3shta2007@yandex.ru>
2021-10-06 10:20:43 +0300

    решено

--- tasks/medium/char_counter.py
+++ tasks/medium/char_counter.py
@@ -14,3 +14,14 @@
 Нельзя пользоваться collections.Counter!
 """
 STR_VAL = 'python is the fastest-growing major programming language'
+
+
+def counts_char(STR_VAL:str) -> dict:
+    counts_dict = {}
+    for char in list(STR_VAL):
+        if char not in counts_dict:
+            counts_dict[char] = 0
+        counts_dict[char] += 1
+    for key, value in counts_dict.items():
+        print(key, value)
+counts_char(STR_VAL)
